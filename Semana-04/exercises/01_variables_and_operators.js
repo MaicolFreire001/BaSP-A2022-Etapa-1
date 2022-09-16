e9e6a6d53f4de88a6a886d574c0b2e1ac787bca9
Maicol Freire <maicolfreire001@gmail.com>
2022-09-16 03:25:19 -0300

    BaSP-W04-2022: Finished exercise 1.b.

--- Semana-04/exercises/01_variables_and_operators.js
+++ Semana-04/exercises/01_variables_and_operators.js
@@ -5,3 +5,9 @@ var variable2 = 15;
 var suma = variable1 + variable2;
 console.log("La suma es:", suma);
 
+/* Crear dos variables de tipo String y concatenarlas guardando el resultado en 
+una 3er variable. */
+var variable1 = "Hello ";
+var variable2 = "World";
+var concatenada = variable1 + variable2;
+console.log("Las variables concatenadas quedan:", concatenada);
