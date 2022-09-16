0d490b349484a8b4367844d6a74cf3d430c3724e
Maicol Freire <maicolfreire001@gmail.com>
2022-09-16 03:40:43 -0300

    BaSP-W04-2022: Finished exercise 2.c.

--- Semana-04/exercises/02_strings.js
+++ Semana-04/exercises/02_strings.js
@@ -6,6 +6,14 @@ console.log("El texto en mayúscula es:", texto.toUpperCase(texto));
 /* 2.b. Crear una variable de tipo string con al menos 10 caracteres y generar
  un nuevo string con los primeros 5 caracteres guardando el resultado en una 
  nueva variable (utilizar substring). */
- var texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
- var primerosCincoCaracteres = texto.substring(0, 5);
+var texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+var primerosCincoCaracteres = texto.substring(0, 5);
 console.log("Los primeros cinco caracteres son:", primerosCincoCaracteres);
+
+/* 2.c. Crear una variable de tipo string con al menos 10 caracteres y generar un 
+nuevo string con los últimos 3 caracteres guardando el resultado en una nueva 
+variable (utilizar substring). */
+var texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+var ultimosTresCaracteres = texto.substring(texto.length-3);
+console.log("Los ultimos tres caracteres son:", ultimosTresCaracteres);
+
