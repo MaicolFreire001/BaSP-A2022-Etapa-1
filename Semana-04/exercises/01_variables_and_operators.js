cd69ff5c9e2b05aed0cf0772e81d0333790a6e9b
Maicol Freire <maicolfreire001@gmail.com>
2022-09-16 03:22:42 -0300

    BaSP-W04-2022: Finished exercise 1.a.

---
+++ Semana-04/exercises/01_variables_and_operators.js
@@ -0,0 +1,7 @@
+/* Crear dos variables numéricas y utilizar el operador suma para guardar 
+el valor de la suma de ambos números en una 3er variable. */
+var variable1 = 10;
+var variable2 = 15;
+var suma = variable1 + variable2;
+console.log("La suma es:", suma);
+
