bc0bb0b9f4ad5444846dc5a7e8bbacf492765ce2
Maicol Freire <maicolfreire001@gmail.com>
2022-09-16 03:33:45 -0300

    BaSP-W04-2022: Finished exercise 2.a.

---
+++ Semana-04/exercises/02_strings.js
@@ -0,0 +1,4 @@
+/* 2.a. Crear una variable de tipo string con al menos 10 caracteres y 
+convertir todo el texto en mayúscula (utilizar toUpperCase). */
+var texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+console.log("El texto en mayúscula es:", texto.toUpperCase(texto));
