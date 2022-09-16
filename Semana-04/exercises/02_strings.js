784a935e8587da65b6e15e2bef13f6bb786db8c8
Maicol Freire <maicolfreire001@gmail.com>
2022-09-16 03:36:29 -0300

    BaSP-W04-2022: Finished exercise 2.b.

--- Semana-04/exercises/02_strings.js
+++ Semana-04/exercises/02_strings.js
@@ -2,3 +2,10 @@
 convertir todo el texto en mayúscula (utilizar toUpperCase). */
 var texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
 console.log("El texto en mayúscula es:", texto.toUpperCase(texto));
+
+/* 2.b. Crear una variable de tipo string con al menos 10 caracteres y generar
+ un nuevo string con los primeros 5 caracteres guardando el resultado en una 
+ nueva variable (utilizar substring). */
+ var texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+ var primerosCincoCaracteres = texto.substring(0, 5);
+console.log("Los primeros cinco caracteres son:", primerosCincoCaracteres);
