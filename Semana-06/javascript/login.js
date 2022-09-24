5896aa92fed04ae7e692b818703fb160aa8b7723
Maicol Freire <maicolfreire001@gmail.com>
2022-09-24 19:03:51 -0300

    BaSP-W06-2022: Added email format verification to login

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -1,10 +1,24 @@
 window.onload = function() {
+    var emailInput = document.getElementById("email");
+    var validEmail = false;
 
+    emailInput.onblur = function() {
+        if(validateEmail(emailInput.value)){
+            emailInput.classList.add("green-border");
+            validEmail = true;
+        }else{
+            emailInput.classList.add("red-border");
+            validEmail = false;
+        }
+    }
 
-
+    emailInput.onfocus = function() {
+        emailInput.classList.remove("green-border");
+        emailInput.classList.remove("red-border");
+    }
 }
 
 function validateEmail(emailText){
     var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
-    return emailText.value.match(emailExpression);
+    return emailText.match(emailExpression);
 }
