7f70be7d3877d655a6a2c8c4a82ecf34c210afd1
Maicol Freire <maicolfreire001@gmail.com>
2022-09-24 20:23:01 -0300

    BaSP-W06-2022: Added password verification for login

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     var emailInput = document.getElementById("email");
     var validEmail = false;
+    var passwordInput = document.getElementById("password");
 
     emailInput.onblur = function() {
         if(validateEmail(emailInput.value)){
@@ -11,14 +12,49 @@ window.onload = function() {
             validEmail = false;
         }
     }
-
     emailInput.onfocus = function() {
         emailInput.classList.remove("green-border");
         emailInput.classList.remove("red-border");
+    }
+    passwordInput.onblur = function() {
+        if(passwordVerification(passwordInput.value)){
+            passwordInput.classList.add("green-border");
+        }else{
+            passwordInput.classList.add("red-border");
+        }
+    }
+    passwordInput.onfocus = function() {
+        passwordInput.classList.remove("green-border");
+        passwordInput.classList.remove("red-border");
     }
 }
 
 function validateEmail(emailText){
     var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
     return emailExpression.test(emailText);
+}
+function passwordVerification(passwordText){
+    if(passwordText.length >= 8){
+        console.log(passwordText);
+        return !hasSpecialChar(passwordText);
+    }else{
+        return false;
+    }
+}
+function hasSpecialChar(text){
+    if(text.includes(" ") || text.includes("`") || text.includes("!") ||
+    text.includes("@")|| text.includes("#") || text.includes("$") ||
+    text.includes("%")|| text.includes("^") || text.includes("&") ||
+    text.includes("*")|| text.includes("(") || text.includes(")") ||
+    text.includes("_")|| text.includes("+") || text.includes("\\") ||
+    text.includes("-")|| text.includes("=") || text.includes("[") ||
+    text.includes("]")|| text.includes("{") || text.includes("}") ||
+    text.includes(";")|| text.includes("'") || text.includes(":") ||
+    text.includes("\"")|| text.includes("|") || text.includes(",") ||
+    text.includes(".")|| text.includes("<") || text.includes(">") ||
+    text.includes("?")|| text.includes("~")){
+        return true;
+    }else{
+        return false;
+    }
 }
